cf7670b02663fe0cb48ee1215eb4bd4d2535f267
Crischelle Polley <crischellepolley@gmail.com>
2024-07-21 06:44:15 -0500

    Update stats container and games container

--- index.js
+++ index.js
@@ -29,26 +29,42 @@ const gamesContainer = document.getElementById("games-container");
 function addGamesToPage(games) {
 
     // loop over each item in the data
-
+    for (const game of games) {
 
         // create a new div element, which will become the game card
-
+        const newDiv = document.createElement('div');
+        console.log('created div');
 
         // add the class game-card to the list
-
+        newDiv.classList.add('game-card');
+        console.log('added class');
 
         // set the inner HTML using a template literal to display some info 
         // about each game
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
-
+        const gameInfo = `
+            <img class='game-img' src='${game.img}'/>
+            <p>${game.name}</p>
+            <p>${game.description}</p>
+            <p>$${game.goal.toLocaleString()} goal</p>
+            <p>${game.pledged.toLocaleString()} pledged</p>
+            <p>${game.backers.toLocaleString()} backers</p>
+            `;
+        newDiv.innerHTML = gameInfo;
+        console.log('set div info');
+        console.log(gameInfo);
 
         // append the game to the games-container
+        gamesContainer.append(newDiv);
+        console.log('appended div to games container');
+    }        
 
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
+addGamesToPage(GAMES_JSON);
 
 
 /*************************************************************************************
